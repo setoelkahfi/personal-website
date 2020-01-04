fb1195d3b83918bfcd1ccfe71edd260512a288d5
Seto Elkahfi <seto@isolve.se>
2020-01-04 23:03:00 +0100

    add medium link

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -30,6 +30,7 @@ const FooterLinks = () => (
     <footer style={footerStyle}>
         <a style={linkStyle} href="https://stackoverflow.com/users/1137814/seto" alt="StackOverflow" target="_blank" rel="noopener noreferrer">stackoverflow</a>
 		<a style={linkStyle} href="https://github.com/setoelkahfi" alt="GitHub" target="_blank" rel="noopener noreferrer">github</a>
+		<a style={linkStyle} href="https://medium.com/@setoelkahfi" alt="Medium" target="_blank" rel="noopener noreferrer">medium</a>
 		<a style={linkStyle} href="https://id.linkedin.com/in/setoelkahfi" alt="LinkedIn" target="_blank" rel="noopener noreferrer">linkedin</a>
     </footer>
 );
