d89f41cb41a5f419a9cd0cad43d5bdfb3a066c82
Seto Elkahfi <setoelkahfi@gmail.com>
2020-01-26 00:59:58 +0100

    Fix IntlProvider throw console error
    The internationalization provider needs the locale information in order to work properly. The locale setup uses domain-based detection. Since the test run without domain-based context, we need to tell the component manually which locale is used.

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { IntlProvider } from 'react-intl';
 import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>, div);
+    <IntlProvider locale="en">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </IntlProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
