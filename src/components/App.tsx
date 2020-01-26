f2613ea8d650ae31f08d5a2248821412bd5f061a
Seto Elkahfi <setoelkahfi@gmail.com>
2020-01-26 03:20:09 +0100

    Simplify index.tsx file
    Move the logic to detect locale to App component so that it can be test thoroughly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,38 @@ import Main from './Main';
 import FooterLinks from './FooterLinks';
 import { CSSProperties } from 'react';
 
-const mainStyle: CSSProperties = { 
+import { BrowserRouter } from 'react-router-dom';
+import Firebase, { FirebaseContext } from './Firebase';
+import { IntlProvider } from 'react-intl';
+import { addLocaleData } from "react-intl";
+import locale_en from 'react-intl/locale-data/en';
+import locale_se from 'react-intl/locale-data/se';
+import locale_id from 'react-intl/locale-data/id';
+import messages_se from "./translations/se.json";
+import messages_id from "./translations/id.json";
+import messages_en from "./translations/en.json";
+
+addLocaleData([...locale_en, ...locale_id, ...locale_se]);
+
+const messages: {
+	[key: string]: object,
+} = {
+	"se": messages_se,
+	"id": messages_id,
+	"en": messages_en
+};
+
+
+var language = "en";
+const tld = window.location.hostname.split('.').pop();
+
+if (tld === "id") {
+	language = "id";
+} else if (tld === "se") {
+	language = "se";
+}
+
+const mainStyle: CSSProperties = {
 	position: 'absolute',
 	top: 50,
 	left: 50,
@@ -15,10 +46,16 @@ const mainStyle: CSSProperties = {
 };
 
 const App = () =>
-    <div style={mainStyle}>
-      	<Header />
-      	<Main />
-		<FooterLinks />
-    </div>;
+	<IntlProvider locale={language} messages={messages[language]}>
+		<BrowserRouter>
+			<FirebaseContext.Provider value={new Firebase(language)}>
+				<div style={mainStyle}>
+					<Header />
+					<Main />
+					<FooterLinks />
+				</div>
+			</FirebaseContext.Provider>
+		</BrowserRouter>
+	</IntlProvider>;
 
 export default App;
--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import { IntlProvider } from 'react-intl';
 import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
-    <IntlProvider locale="en">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </IntlProvider>, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
